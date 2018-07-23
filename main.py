95c5918cde7973a1816921f6760bdcac1c98b67a
Demo User <demouser@MacBook-Pro-5.local>
2018-07-23 11:11:56 -0400

    jkw

---
+++ main.py
@@ -0,0 +1,25 @@
+
+import webapp2
+import os
+import jinja2
+from google.appengine.ext import ndb
+
+
+
+jinja_current_dir = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        template= jinja_current_dir.get_template('my_blog.html')
+        self.response.write(template.render())
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/aboutme', AboutMeHandler),
+    ('/posts', PostsHandler),
+], debug=True)
