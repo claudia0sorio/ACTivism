041df0a1d5ca79d9ac2cced683805753c55903ac
Demo User <demouser@MacBook-Pro-5.local>
2018-07-23 11:08:00 -0400

    sdakjfkj

--- main.py
+++ main.py
@@ -4,72 +4,32 @@ import os
 import jinja2
 from google.appengine.ext import ndb
 
-<<<<<<< HEAD
-from django import template
 
-from calendar import Calendar
-import datetime
 
-import re
-=======
-
->>>>>>> 3714e49e0c603a8921bfdb94994f70cb6e9f2829
 
 jinja_current_dir = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-<<<<<<< HEAD
-
-register = template.Library()
-
-@register.tag(name="get_calendar")
-def do_calendar(parser, token):
-    syntax_help = "syntax should be \"get_calendar for <month> <year> as <var_name>\""
-    # This version uses a regular expression to parse tag contents.
-    try:
-        # Splitting by None == splitting by spaces.
-        tag_name, arg = token.contents.split(None, 1)
-    except ValueError:
-        raise template.TemplateSyntaxError, "%r tag requires arguments, %s" % (token.contents.split()[0], syntax_help)
-    m = re.search(r'for (.*?) (.*?) as (\w+)', arg)
-    if not m:
-        raise template.TemplateSyntaxError, "%r tag had invalid arguments, %s" % (tag_name, syntax_help)
-
-    return GetCalendarNode(*m.groups())
-
-class GetCalendarNode(template.Node):
-    def __init__(self, month, year, var_name):
-        self.year = template.Variable(year)
-        self.month = template.Variable(month)
-        self.var_name = var_name
-
-    def render(self, context):
-        mycal = Calendar()
-        context[self.var_name] = mycal.monthdatescalendar(int(self.year.resolve(context)), int(self.month.resolve(context)))
-
-        return ''
 class CalendarHandler(webapp2.RequestHandler):
     def get(self):
+        template= jinja_current_dir.get_template('blank.html')
+        self.response.write(template.render())
 
 
 
 
-
-class HomePageHandler(webapp2.RequestHandler):
-    def get(self):
-
-
+#class HomePageHandler(webapp2.RequestHandler):
+#    def get(self):
 
 
 
 
 app = webapp2.WSGIApplication([
     ('/calendar', CalendarHandler),
-    ('/', HomePageHandler),
 ], debug=True)
-=======
+
 
 
 
@@ -77,7 +37,7 @@ app = webapp2.WSGIApplication([
     # ('/calendar', CalendarHandler),
     # ('/', HomePageHandler),
 # ], debug=True)
->>>>>>> 3714e49e0c603a8921bfdb94994f70cb6e9f2829
+
 
 
 
--- static/scripts/calendar.js
+++ static/scripts/calendar.js
@@ -2,5 +2,5 @@ var winHeight = $(window).height();
 var height = ( winHeight * 16.6666 ) / 100;
 var lineHeight = height + "px";
 
-$("li").css("line-height", lineHeight);
-$("li").css("height", height);
+("li").css("line-height", lineHeight);
+("li").css("height", height);
