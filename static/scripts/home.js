785714313d633698794e2b624078b27f3b0d9f20
claudia osorio <demouser@MacBook-Pro.local>
2018-07-24 10:56:58 -0400

    new slideshow

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,10 +1,12 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
+// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -14,13 +16,13 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
+  }
+  for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
-    }
+  }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
